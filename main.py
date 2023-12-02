f24a0a8d8c74f627ffad297913b907a044371feb
Ankit Tripathi <94532785+at1609@users.noreply.github.com>
2023-12-02 20:30:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
-from fastapi import FastAPI, Request, Form, Depends
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Request, Form
 import sqlite3
 import pandas as pd
-import os
-from dotenv import load_dotenv
 from langchain.llms import OpenAI
 from langchain.utilities import WikipediaAPIWrapper
-from langchain.prompts import PromptTemplate
 from langchain.memory import ConversationBufferMemory
-from langchain.chains import LLMChain
 from langchain.agents import Tool
 from langchain.agents import AgentType
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
-from langchain import OpenAI
-from langchain.agents import initialize_agent
 from langchain.tools import DuckDuckGoSearchRun
-from langchain.utilities import WikipediaAPIWrapper
-from langchain.tools import YouTubeSearchTool
+from langchain.chains import Chroma, RetrievalQA
 from langchain.vectorstores import Chroma
-from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import LLMChain
+from langchain.agents import initialize_agent
 
 app = FastAPI()
-
-templates = Jinja2Templates(directory="templates")
 
 
 def create_research_db():
@@ -107,24 +99,28 @@ def generate_research(userInput, TEMP):
                     readings['output'])
 
 
-@app.get("/")
-def read_item(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-
 @app.post("/generate_report")
-def generate_report(request: Request, user_input: str = Form(...), TEMP: float = Form(...)):
+def generate_report(user_input: str = Form(...), TEMP: float = Form(...)):
     generate_research(user_input, TEMP)
     research_df = read_research_table().tail(1)
     documents = research_df.apply(
-        lambda row: Document(' '.join([f'{idx}: {val}' for idx, val in zip(row.index, row.values.astype(str))]),
-                             row['user_input']), axis=1).tolist()
+        lambda row: {'user_input': row['user_input'],
+                     'introduction': row['introduction'],
+                     'quant_facts': row['quant_facts'],
+                     'publications': row['publications'],
+                     'books': row['books']}, axis=1).tolist()
     embeddings_db = Chroma.from_documents(documents, OpenAIEmbeddings(), persist_directory="./chroma_db")
     embeddings_db.persist()
-    return templates.TemplateResponse("report_generated.html", {"request": request, "user_input": user_input})
+    return {"status": "Report generated successfully", "user_input": user_input}
 
 
 @app.get("/previous_research")
-def previous_research(request: Request):
-    df = read_research_table()
-    return templates.TemplateResponse("previous_research.html", {"request": request, "df": df})
+def previous_research():
+    df = read_research_table().to_dict(orient="records")
+    return df
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
+
