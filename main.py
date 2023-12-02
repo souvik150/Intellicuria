35ca31a9b5902b9d5630e073ebbb24f4a98b5278
Ankit Tripathi <94532785+at1609@users.noreply.github.com>
2023-12-02 15:55:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-from fastapi import FastAPI, Request, Form
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel
+from flask import Flask, request, jsonify
 from langchain.llms import OpenAI
-from langchain.utilities import WikipediaAPIWrapper
-from langchain.tools import DuckDuckGoSearchRun
-from langchain.tools import YouTubeSearchTool
-from langchain.agents import Tool, AgentType, initialize_agent
+from langchain.utilities import WikipediaAPIWrapper 
+from langchain.prompts import PromptTemplate
 from langchain.memory import ConversationBufferMemory
-from langchain.chains import RetrievalQA
+from langchain.chains import LLMChain
+from langchain.agents import Tool
+from langchain.agents import AgentType
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain import OpenAI
+from langchain.agents import initialize_agent
+from langchain.tools import DuckDuckGoSearchRun
+from langchain.utilities import WikipediaAPIWrapper
+from langchain.tools import YouTubeSearchTool
 from langchain.vectorstores import Chroma
-from youtubesearchpython import VideosSearch
+from langchain.chains import RetrievalQA
 import sqlite3
 import pandas as pd
 import os
 
-app = FastAPI()
-
-
-class ResearchRequest(BaseModel):
-    userInput: str
-
+app = Flask(__name__)
 
 def create_research_db():
     with sqlite3.connect('MASTER.db') as conn:
@@ -30,11 +30,9 @@ def create_research_db():
                 introduction TEXT,
                 quant_facts TEXT,
                 publications TEXT,
-                books TEXT,
-                ytlinks TEXT
+                books TEXT
             )
         """)
-
 
 def read_research_table():
     with sqlite3.connect('MASTER.db') as conn:
@@ -42,43 +40,36 @@ def read_research_table():
         df = pd.read_sql_query(query, conn)
     return df
 
-
-def insert_research(user_input, introduction, quant_facts, publications, books, ytlinks):
+def insert_research(user_input, introduction, quant_facts, publications, books):
     with sqlite3.connect('MASTER.db') as conn:
         cursor = conn.cursor()
         cursor.execute("""
-            INSERT INTO Research (user_input, introduction, quant_facts, publications, books, ytlinks)
-            VALUES (?, ?, ?, ?, ?, ?)
-        """, (user_input, introduction, quant_facts, publications, books, ytlinks))
-
+            INSERT INTO Research (user_input, introduction, quant_facts, publications, books)
+            VALUES (?, ?, ?, ?, ?)
+        """, (user_input, introduction, quant_facts, publications, books))
 
 def generate_research(user_input):
     llm = OpenAI(temperature=TEMP)
     wiki = WikipediaAPIWrapper()
     DDGsearch = DuckDuckGoSearchRun()
-    YTsearch = YouTubeSearchTool()
     tools = [
         Tool(
             name="Wikipedia Research Tool",
             func=wiki.run,
-            description="Useful for researching information on wikipedia"
+            description="Useful for researching information on Wikipedia"
         ),
         Tool(
             name="Duck Duck Go Search Results Tool",
             func=DDGsearch.run,
-            description="Useful for search for information on the internet"
-        ),
-        Tool(
-            name="YouTube Search Tool",
-            func=YTsearch.run,
-            description="Useful for gathering links on YouTube"
+            description="Useful for searching for information on the internet"
         )
     ]
 
-    if st.session_state.embeddings_db:
-        qa = RetrievalQA.from_chain_type(llm=llm,
-                                         chain_type="stuff",
-                                         retriever=st.session_state.embeddings_db.as_retriever())
+    # Replace st.session_state with a global variable or a class instance if necessary
+    # If using a class, make sure to update it in the functions accordingly.
+
+    if os.path.exists("./chroma_db"):
+        embeddings_db = Chroma(persist_directory="./chroma_db", embedding_function=embedding_function)
         tools.append(
             Tool(
                 name='Previous Research Database Tool',
@@ -86,14 +77,13 @@ def generate_research(user_input):
                 description="Useful for looking up previous research/information"
             )
         )
-
     memory = ConversationBufferMemory(memory_key="chat_history")
     runAgent = initialize_agent(tools,
                                 llm,
                                 agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION,
                                 verbose=True,
                                 memory=memory,
-                                )
+                                handle_parsing_errors=True)
 
     intro = runAgent(f'Write an academic introduction about {user_input}')
     quant_facts = runAgent(f'''
@@ -114,32 +104,37 @@ def generate_research(user_input):
         \nConsider these quantitative facts "{quant_facts}"
         \nNow Generate a list of 5 relevant books to read relating to {user_input}.
     ''')
-    search = VideosSearch(user_input)
-    ytlinks = ""
-    for i in range(1, 6):
-        ytlinks += (str(i) + ". Title: " + search.result()['result'][0]['title'] + "Link: https://www.youtube.com/watch?v=" + search.result()['result'][0]['id'] + "\n")
-        search.next()
 
-    insert_research(user_input,
-                    intro['output'],
-                    quant_facts['output'],
-                    papers['output'],
-                    readings['output'],
-                    ytlinks)
+    insert_research(user_input, intro['output'], quant_facts['output'], papers['output'], readings['output'])
 
+@app.route('/generate_research', methods=['POST'])
+def generate_research_endpoint():
+    data = request.get_json()
+    user_input = data.get('user_input')
 
-class Document:
-    def __init__(self, content, topic):
-        self.page_content = content
-        self.metadata = {"Topic": topic}
+    if not user_input:
+        return jsonify({"error": "User input is missing"}), 400
 
+    try:
+        generate_research(user_input)
+        research_df = read_research_table().tail(1)
+        documents = research_df.apply(lambda row: Document(' '.join([f'{idx}: {val}' for idx, val in zip(row.index, row.values.astype(str))]), row['user_input']), axis=1).tolist()
+        embeddings_db = Chroma.from_documents(documents, embedding_function, persist_directory="./chroma_db")
+        embeddings_db.persist()
 
-@app.post("/generate_research/", response_model=dict)
-def generate_research_api(request: ResearchRequest):
-    user_input = request.userInput
-    generate_research(user_input)
-    research_df = read_research_table().tail(1)
-    return {"message": "Research generated successfully", "data": research_df.to_dict(orient='records')}
+        return jsonify({"success": True, "message": "Research generated successfully"})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/get_previous_research', methods=['GET'])
+def get_previous_research_endpoint():
+    research_data = read_research_table().to_dict(orient='records')
+    return jsonify(research_data)
+
+if __name__ == '__main__':
+    load_dotenv()
+    app.run(debug=True)
+
 
 
 @app.get("/previous_research/", response_model=dict)
